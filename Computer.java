1dd5fd18fb2c1a19c528af7e3556d9bc853c7b5b
dxmdxm123 <87161000+dxmdxm123@users.noreply.github.com>
2021-08-31 11:55:51 +0800

    Add files via upload

---
+++ Computer.java
@@ -0,0 +1,10 @@
+package spring08autowire;
+
+public class Computer implements GameInterface{
+    public Computer(){
+        System.out.println("电脑对象出现");
+    }
+    public void game(){
+        System.out.println("电脑运行  cf");
+    }
+}
